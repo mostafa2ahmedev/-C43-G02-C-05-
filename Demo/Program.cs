e8e97a125f4eb8efb2133a501238a2e65632adee
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-22 10:15:35 +0200

    Q19

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Demo
+﻿using System.Security.Principal;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q19)Write a program that prints an identity matrix using for loop, inother words takes a value n from the user and shows the identity table of size n* n.
+            //int number;
+            //bool parsed;
+            //do
+            //{
+            //    Console.Write("Enter a number more than 1 that will be used for the size of the matrix : ");
+            //  parsed=  int.TryParse(Console.ReadLine(), out number);
+            //}
+            //while (!parsed ||number<=1);
+
+            //int[,] arr = new int[number,number];
+
+            //for (int i = 0; i < arr.GetLength(0); i++) // Row
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++) // Column
+            //    {
+            //        if (i == j)
+            //            arr[i,j] = 1;
+            //        else
+            //            arr[i,j] = 0;
+            //        Console.Write(arr[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+
         }
     }
 }
