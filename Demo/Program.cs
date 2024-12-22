2fa2697765406bbe56685d942f91217acc364d4e
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-22 12:52:55 +0200

    Q21

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,42 @@
-﻿using System.Security.Principal;
+﻿using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Demo
 {
     internal class Program
     {
+
+        public static void SwapByValue(int x, int y)
+        {
+            int temp = y;
+            y = x;
+            x = temp;
+        }
+        public static void SwapByRef(ref int x, ref int y)
+        {
+            int temp = y;
+            y = x;
+            x = temp;
+        }
+
+        public static void ChangeChar(int charIndex, char newChar,ref StringBuilder str) {
+
+            str[charIndex] = newChar;
+
+        }
         static void Main(string[] args)
         {
-            #region Q19)Write a program that prints an identity matrix using for loop, inother words takes a value n from the user and shows the identity table of size n* n.
+            #region Q19) Write a program that prints an identity matrix using for loop, inother words takes a value n from the user and shows the identity table of size n* n.
             //int number;
             //bool parsed;
             //do
@@ -35,7 +65,7 @@ namespace Demo
 
             #endregion
 
-            #region Q20)Write a program in C# Sharp to find the sum of all elements of the array.
+            #region Q20) Write a program in C# Sharp to find the sum of all elements of the array.
             //int ArrayLength;
             //bool parsed;
             //do
@@ -64,7 +94,60 @@ namespace Demo
             //Console.WriteLine($"The sum is : {result}");
             #endregion
 
-            
+            #region Q21) Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            //int ArrayLength;
+            //bool parsed;
+            //do
+            //{
+            //    Console.Write("Enter the array length ");
+            //    parsed = int.TryParse(Console.ReadLine(), out ArrayLength);
+            //}
+            //while (ArrayLength <= 0 || !parsed);
+
+            //int[] fArray = new int[ArrayLength];
+            //int[] sArray = new int[ArrayLength];
+            //int[] tArray = new int[2 * ArrayLength];
+
+            //for (int i = 0; i < fArray.Length;)
+            //{
+            //    Console.Write($"Enter an integer values please for the First Array element No {i + 1}  : ");
+            //    parsed = int.TryParse(Console.ReadLine(), out fArray[i]);
+            //    if (parsed)
+            //        i++;
+            //}
+            //Console.WriteLine("-----------------");
+            //for (int i = 0; i < sArray.Length;)
+            //{
+            //    Console.Write($"Enter an integer values please for the Second Array element No {i + 1}  : ");
+            //    parsed = int.TryParse(Console.ReadLine(), out sArray[i]);
+            //    if (parsed)
+            //        i++;
+            //}
+            //for (int i = 0; i < fArray.Length; i++)
+            //{
+            //    tArray[i] = fArray[i]; // Copy first array into total array
+            //}
+            //for (int i = 0; i < sArray.Length; i++)
+            //{
+            //    tArray[fArray.Length + i] = sArray[i]; // Copy second array into total array
+            //}
+
+            //Array.Sort(tArray);  // using built in funtion  [class member function]
+
+            //for (int i = 0; i < tArray.Length; i++)
+            //{
+            //    Console.WriteLine(tArray[i]);
+            //}
+
+
+
+            #endregion
+
+
+ 
+
         }
     }
 }
+    
+
